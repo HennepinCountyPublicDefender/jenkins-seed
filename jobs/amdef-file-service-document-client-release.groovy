44df587401328542324c9b630162fe8a208055c7
“a541676” <jim.beyers@target.com>
2016-02-08 21:36:26 -0800

    removed some creds stuff

--- jobs/amdef-file-service-document-client-release.groovy
+++ jobs/amdef-file-service-document-client-release.groovy
@@ -4,12 +4,8 @@ job('amdef-file-service-document-client-release') {
     },
   wrappers {
       mavenRelease {
-          scmUserEnvVar('hennepinPdoBuilderRobot')
-          scmPasswordEnvVar('MY_PASSWORD_ENV')
           releaseGoals('release:prepare release:perform')
           dryRunGoals('-DdryRun=true release:prepare')
-          selectCustomScmCommentPrefix()
-          selectAppendJenkinsUsername()
           selectScmCredentials()
           numberOfReleaseBuildsToKeep(5)
       }
