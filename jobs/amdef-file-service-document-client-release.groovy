f21ad4f2dc3f1755810062d2c2828d32c034309f
“a541676” <jim.beyers@target.com>
2016-02-08 21:48:59 -0800

    fixed the formatting

--- jobs/amdef-file-service-document-client-release.groovy
+++ jobs/amdef-file-service-document-client-release.groovy
@@ -1,7 +1,7 @@
 job('amdef-file-service-document-client-release') {
-    scm {
-        git('git@github.com:HennepinCountyPublicDefender/amdef-file-service-document-client.git')
-    },
+  scm {
+      git('git@github.com:HennepinCountyPublicDefender/amdef-file-service-document-client.git')
+  }
   wrappers {
       mavenRelease {
           releaseGoals('release:prepare release:perform')
