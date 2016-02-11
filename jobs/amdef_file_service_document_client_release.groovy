af22036121a2856e4256084be12c2f3c84c7da71
“a541676” <jim.beyers@target.com>
2016-02-10 20:55:26 -0800

    dashes to underscores in files themselves

---
+++ jobs/amdef_file_service_document_client_release.groovy
@@ -0,0 +1,13 @@
+mavenJob('amdef_file_service_document_client_release') {
+  scm {
+      git('git@github.com:HennepinCountyPublicDefender/amdef-file-service-document-client.git')
+  }
+  wrappers {
+      mavenRelease {
+          releaseGoals('release:prepare release:perform')
+          dryRunGoals('-DdryRun=true release:prepare')
+          selectScmCredentials()
+          numberOfReleaseBuildsToKeep(5)
+      }
+  }
+}
