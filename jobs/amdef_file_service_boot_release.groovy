62cb82750432d09326b183364bb4e290972d7efc
“a541676” <jim.beyers@target.com>
2017-05-23 22:29:15 -0700

    fixed again I hope with the security off?

--- jobs/amdef_file_service_boot_release.groovy
+++ jobs/amdef_file_service_boot_release.groovy
@@ -1,8 +1,18 @@
 mavenJob('amdef_file_service_boot_release') {
   jdk('Java 8')
   scm {
-      git('https://github.com/HennepinCountyPublicDefender/amdef-file-service-boot.git','*/master')
-  }
+      git {
+            remote {
+                    name('origin')
+                    url('https://github.com/HennepinCountyPublicDefender/amdef-file-service-boot.git')
+                    }
+            branch('master')
+            extensions {
+                    wipeOutWorkspace()
+                    localBranch('master')
+                    }
+            }
+        }
   wrappers {
       mavenRelease {
           releaseGoals('release:prepare release:perform')
