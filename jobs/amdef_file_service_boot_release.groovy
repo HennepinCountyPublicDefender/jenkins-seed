020ecc88b658848488e2a135bfa439d8d2557c28
“a541676” <jim.beyers@target.com>
2017-05-23 21:23:15 -0700

    added release for boot

---
+++ jobs/amdef_file_service_boot_release.groovy
@@ -0,0 +1,14 @@
+mavenJob('amdef_file_service_boot_release') {
+  jdk('Java 8')
+  scm {
+      git('https://github.com/HennepinCountyPublicDefender/amdef-file-service-boot.git','*/master')
+  }
+  wrappers {
+      mavenRelease {
+          releaseGoals('release:prepare release:perform')
+          dryRunGoals('-DdryRun=true release:prepare')
+          selectScmCredentials(false)
+          numberOfReleaseBuildsToKeep(5)
+      }
+  }
+}
